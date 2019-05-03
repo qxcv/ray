dd9a9640c43542a320babac0749ae4f72d9dbc2d
Sam Toyer <sam@qxcv.net>
2019-05-03 10:24:28 -0700

    More expt scripts

--- python/ray/rllib/examples/gail/runs-halfcheetah.sh
+++ python/ray/rllib/examples/gail/runs-halfcheetah.sh
@@ -3,6 +3,10 @@
 # Hacky script to run HalfCheetah with a bunch of configs. I used this to make
 # my poster plots.
 
-python gail.py with halfcheetah-full.yaml
-python gail.py with halfcheetah-full-nogpu-one-worker.yaml
-python gail.py with halfcheetah-full-nogpu.yaml
+for iter in {1..4}; do
+    echo "Running iteration ${iter}"
+    python gail.py with halfcheetah-full.yaml
+    python gail.py with halfcheetah-full-nogpu-one-worker.yaml
+    python gail.py with halfcheetah-full-nogpu.yaml
+    python gail.py with halfcheetah-sync.yaml
+done
